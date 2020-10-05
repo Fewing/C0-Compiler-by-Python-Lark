b75a8fde32bc49fbc95a1a14ccc31ea502157961
Fewing <yangzheyu98@gmail.com>
2020-10-05 20:47:08 +0800

    修正EOF

--- lex2.py
+++ lex2.py
@@ -18,24 +18,29 @@ keywords[':='] = 'Assign'
 
 
 if __name__ == "__main__":
-    while True:
-        current_line = input()
-        if not current_line:
-            break
-        token_list = current_line.split()
-        for token in token_list:
-            if token in keywords.keys():
-                print(keywords[token])
-            elif token.isdigit():
-                print(f'Int({int(token)})')
-            elif token[0].isalpha() and token.isalnum():
-                print(f'Ident({token})')
-            elif token[0].isdigit() and token.isalnum():
-                for ch in token:
-                    if ch.isalpha():
-                        start = token.find(ch)
-                        print(f'Int({int(token[:start])})')
-                        print(f'Ident({token[start:]})')
-                        break
-            elif token.isspace():
-                pass
+    try:
+        while True:
+            current_line = input()
+            if not current_line:
+                break
+            token_list = current_line.split()
+            for token in token_list:
+                if token in keywords.keys():
+                    print(keywords[token])
+                elif token.isdigit():
+                    print(f'Int({int(token)})')
+                elif token[0].isalpha() and token.isalnum():
+                    print(f'Ident({token})')
+                elif token[0].isdigit() and token.isalnum():
+                    for ch in token:
+                        if ch.isalpha():
+                            start = token.find(ch)
+                            print(f'Int({int(token[:start])})')
+                            print(f'Ident({token[start:]})')
+                            break
+                elif token.isspace():
+                    pass
+                else:
+                    break
+    except EOFError:
+        pass
