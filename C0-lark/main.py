4007b1c1f7a09e625e76419c36eea8488c509ab6
Fewing <yangzheyu98@gmail.com>
2020-11-27 17:09:17 +0800

    表达式和语句

--- C0-lark/main.py
+++ C0-lark/main.py
@@ -3,5 +3,9 @@ from lark import Lark
 f_lark = open('test.lark')
 lark = f_lark.read()
 l = Lark(lark)
-res = l.parse(" a + 1 * ( 4 + 2 ) >= -1 + 2 * a + 4")
+res = l.parse('''
+if x > 0 {
+  x = x + 1;
+}
+''')
 print(res.pretty())
