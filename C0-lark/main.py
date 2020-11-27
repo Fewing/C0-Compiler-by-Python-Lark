33a3fcdc57755418c0fae33ad13d6bfb1d455767
Fewing <yangzheyu98@gmail.com>
2020-11-27 19:10:04 +0800

    貌似词法语法写完了？

--- C0-lark/main.py
+++ C0-lark/main.py
@@ -4,8 +4,27 @@ f_lark = open('test.lark')
 lark = f_lark.read()
 l = Lark(lark)
 res = l.parse('''
-if x > 0 {
-  x = x + 1;
+fn fib(x: int) -> int {
+    if x<=1 {
+        return 1;
+    }
+    let result: int = fib(x - 1);
+    result = result + fib(x - 2);
+    return result;
+}
+
+fn main() -> int {
+    let i: int = 0;
+    let j: int;
+    j = getint();
+    while i < j {
+        putint(i);
+        putchar(32);
+        putint(fib(i));
+        putln();
+        i = i + 1;
+    }
+    return 0;
 }
 ''')
 print(res.pretty())
