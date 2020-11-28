0df112e754f8c7daf08e4ade21599694fffb2af9
Fewing <yangzheyu98@gmail.com>
2020-11-28 15:38:14 +0800

    更新main

--- C0-lark/main.py
+++ C0-lark/main.py
@@ -1,5 +1,25 @@
-from lark import Lark
+from lark import Lark ,Tree
 import sys
+
+import utils.postfunc as postfn
+import utils.prefunc as prefn
+
+def codegen(tree: Tree):
+    if isinstance(tree, Tree):
+        if tree.data == 'block_stmt':
+            prefn.block_stmt(tree)
+        if tree.data == 'let_decl_stmt':
+            prefn.let_decl_stmt(tree)
+        if tree.data == 'const_decl_stmt':
+            prefn.const_decl_stmt(tree)
+        if tree.data == 'function':
+            prefn.function(tree)
+        for child in tree.children:
+            codegen(child)
+        if tree.data == 'block_stmt':
+            postfn.block_stmt(tree)
+    else:
+        pass
 
 if __name__ == "__main__":
     input_path = sys.argv[1]
@@ -13,8 +33,9 @@ if __name__ == "__main__":
     lark_str = lark_file.read()
     lark = Lark(lark_str)
 
-    res = lark.parse(input_str)
-    ouput_file.write(res.pretty())
+    parse_tree = lark.parse(input_str)
+    codegen(parse_tree)
+    ouput_file.write(parse_tree.pretty())
 
     input_file.close()
     ouput_file.close()
