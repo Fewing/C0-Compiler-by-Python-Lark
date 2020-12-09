e788730cb201dae041808e0a2df4f0cd49e9e925
Fewing <yangzheyu98@gmail.com>
2020-12-09 18:11:20 +0800

    精简语法

--- C0-lark/test.py
+++ C0-lark/test.py
@@ -14,6 +14,7 @@ if __name__ == "__main__":
     lark_str = lark_file.read()
     lark = Lark(lark_str)
     parse_tree = lark.parse(input_str)
+    print(parse_tree.pretty())
     # 代码生成
     gen = Generator()
     gen.codegen(parse_tree)
